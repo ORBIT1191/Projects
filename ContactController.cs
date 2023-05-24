f63860bc6e0d38a92371e0316433a00fb4ff3be2
ORBIT1191 <86366789+ORBIT1191@users.noreply.github.com>
2023-05-24 11:10:10 +0530

    Add files via upload

---
+++ ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class ContactController : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
